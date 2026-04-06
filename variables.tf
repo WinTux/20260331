b28a5c92693e4a3795848f8cf6d1815ce6d50550
rusok <rusok.software@gmail.com>
2026-04-06 11:44:33 -0400

    Corrección de configuración

--- variables.tf
+++ variables.tf
@@ -3,6 +3,6 @@ variable "aws_region" {
   default   = "us-east-1"
 }
 variable "ruta_private_key" {
-  description "Ruta del archivo PEM para conexión SSH."
+  description = "Ruta del archivo PEM para conexión SSH."
   type = string
 }
